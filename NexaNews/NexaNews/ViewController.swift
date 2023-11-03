410dea6fafc7d945db79e378e9e03dd6a706a324
Jesse Hough <jessehough769@gmail.com>
2023-11-02 23:48:26 -0400

    Add Xcode project files

---
+++ NexaNews/NexaNews/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NexaNews
+//
+//  Created by Jesse Hough on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    
+    @IBOutlet weak var NewsOutletIcon: UIView!
+    
+}
+
